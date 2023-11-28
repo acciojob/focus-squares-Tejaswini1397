6f474dab7771fafd1e31007caebe2dd8e9765a4c
Tejaswini Khairnar <117677072+Tejaswini1397@users.noreply.github.com>
2023-11-28 09:46:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,9 +7,9 @@ document.addEventListener('DOMContentLoaded', function () {
 
   // Function to change the color of all squares to Coffee (#6F4E37)
   function changeColorToCoffee() {
-    square1.style.backgroundColor = '#6F4E37';
-    square2.style.backgroundColor = '#6F4E37';
-    square3.style.backgroundColor = '#6F4E37';
+    // square1.style.backgroundColor = '#6F4E37';
+    // square2.style.backgroundColor = '#6F4E37';
+    // square3.style.backgroundColor = '#6F4E37';
   }
 
   // Add mouseover event listener to each square
